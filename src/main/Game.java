659b1fbfad37d92737bb3a04ef42c2daf8e62e4d
BradpuppyC <rayray21211@gmail.com>
2024-01-16 16:53:48 +0000

    key pressed 2-5.30

--- src/main/Game.java
+++ src/main/Game.java
@@ -8,8 +8,9 @@ public class Game {
     // Constructor: Head method of this Game class, we run any code in here
     //
     public Game() {
-        gameWindow = new GameWindow();
         gamePanel = new GamePanel();
+        gameWindow = new GameWindow(gamePanel);
+        gamePanel.requestFocus();
     }
 
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,18 +1,25 @@
 package main;
 
+import inputs.KeyboardInputs;
+
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 
 // JPanel is the painting inside the frame
 public class GamePanel extends JPanel {
 
     public GamePanel() {
+
+        addKeyListener(new KeyboardInputs());
+
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawRect();
+        g.fillRect(100, 100, 200, 50);
     }
 
 }
