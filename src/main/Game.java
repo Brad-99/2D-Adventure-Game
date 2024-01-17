4ce4ede476e5e6da2ef2b66ec4992c7cde62726d
BradpuppyC <rayray21211@gmail.com>
2024-01-17 12:33:53 +0000

    Click Color Gmae

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,16 +1,50 @@
 package main;
 
-public class Game {
+
+public class Game implements Runnable {
 
     private GameWindow gameWindow; // Creat an object gameWindow of GameWindow Class.
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int FPS_SET = 120;
 
-    // Constructor: Head method of this Game class, we run any code in here
+    // Constructor: Head method of this Game class, any code in here will be run
     //
     public Game() {
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
+   }
+
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 
+    @Override
+    public void run() {
+
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
+        while (true) {
+
+            now = System.nanoTime();
+            if (now - lastFrame >= timePerFrame) {
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+
+            if(System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+
+        }
+    }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,21 +2,32 @@ package main;
 
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
-import java.awt.Graphics;
+
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Random;
 import javax.swing.JPanel;
 
 // JPanel is the painting inside the frame
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 1f, yDir = 1f;
+    private int frames = 0;
+    private long lastCheck = 0;
+    private Color color = new Color(150,20,90);
+    private Random random;
+
+    private ArrayList<MyRect> rects = new ArrayList<>();
 
     public GamePanel() { // Constructor
         // Adding a new instance of KeyboardInputs as a KeyListener
         // "this" is a reference to the current instance of the class. In this case,
         // it's passed to the KeyboardInputs instance so that it knows which GamePanel it is associated with.
+        random = new Random();
         addKeyListener(new KeyboardInputs(this));
         mouseInputs = new MouseInputs(this);
         addMouseListener(mouseInputs);
@@ -25,24 +36,88 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
+
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
+
     }
 
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+    }
+
+    public void spawnRect(int x, int y) {
+        rects.add(new MyRect(x, y));
     }
 
     public void paintComponent(Graphics g) { // This is what's inside the paint
         super.paintComponent(g);
+        //------------------
+        // Temp Rects
+        for(MyRect rect : rects) {
+            rect.updateRect();
+            rect.draw(g);
+        }
+        //------------------
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect((int)xDelta, (int)yDelta, 200, 50);
 
-        g.fillRect(xDelta, yDelta, 200, 50);
+    }
+
+    private void updateRectangle() {
+        xDelta += xDir;
+        if(xDelta > 400 || xDelta < 0) {
+            xDir *= -1;
+            color = getRndColor();
+        }
+        yDelta += yDir;
+        if(yDelta > 400 || yDelta < 0) {
+            yDir *= -1;
+            color = getRndColor();
+        }
+    }
+    private Color getRndColor() {
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+        return new Color(r, b, g);
+    }
+
+    public class MyRect {
+        int x, y, w, h;
+        int xDir = 1, yDir = 1;
+        Color color;
+            public MyRect(int x, int y) {
+                this.x = x;
+                this.y = y;
+                w = random.nextInt(50);
+                h = w;
+                color = newColor();
+            }
+            public void updateRect() {
+                this.x += xDir;
+                this.y += yDir;
+
+                if ((x + w) > 400 || x < 0) {
+                    xDir *= -1;
+                    color = newColor();
+                }
+                if ((y + h) > 400 || y < 0) {
+                    yDir *= -1;
+                    color = newColor();
+                }
+            }
+        private Color newColor() {
+                return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        }
+        public void draw(Graphics g) {
+                g.setColor(color);
+                g.fillRect(x, y, w, h);
+        }
     }
 
 }
