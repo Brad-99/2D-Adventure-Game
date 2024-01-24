2279102ae353f1a344d567705da8a4a972fc3cb2
BradpuppyC <rayray21211@gmail.com>
2024-01-24 12:40:24 +0000

    8done

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,98 +1,100 @@
 package entities;
 
-import utilz.LoadSave;
-
-import java.awt.*;
-import java.awt.image.BufferedImage;
-
 import static utilz.Constants.PlayerConstants.*;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+
+import utilz.LoadSave;
+
 public class Player extends Entity {
-    private int aniTick, aniIndex, aniSpeed = 15;
+    private BufferedImage[][] animations;
+    private int aniTick, aniIndex, aniSpeed = 25;
     private int playerAction = IDLE;
     private boolean moving = false, attacking = false;
     private boolean left, up, right, down;
     private float playerSpeed = 2.0f;
 
-
-
-    private BufferedImage[][] animations;
-    public Player(int x1, int i, float x, float y) {
-        super(x, y);
+    public Player(float x, float y, int width, int height) {
+        super(x, y, width, height);
         loadAnimations();
     }
 
     public void update() {
         updatePos();
-        updateAnimationTic();
+        updateAnimationTick();
         setAnimation();
     }
 
     public void render(Graphics g) {
-        g.drawImage(animations[playerAction][aniIndex], (int) x, (int) y, 256, 160, null);
+        g.drawImage(animations[playerAction][aniIndex], (int) x, (int) y, width, height, null);
     }
 
-    private void updateAnimationTic() {
+    private void updateAnimationTick() {
         aniTick++;
-        if(aniTick >= aniSpeed) {
+        if (aniTick >= aniSpeed) {
             aniTick = 0;
             aniIndex++;
-            if(aniIndex >= GetSpriteAmount(playerAction)) {
+            if (aniIndex >= GetSpriteAmount(playerAction)) {
                 aniIndex = 0;
                 attacking = false;
             }
+
         }
+
     }
 
     private void setAnimation() {
-
         int startAni = playerAction;
 
-        if(moving)
+        if (moving)
             playerAction = RUNNING;
         else
             playerAction = IDLE;
-        if(attacking)
+
+        if (attacking)
             playerAction = ATTACK_1;
 
-        if(startAni != playerAction)
+        if (startAni != playerAction)
             resetAniTick();
     }
 
     private void resetAniTick() {
-        aniTick=0;
+        aniTick = 0;
         aniIndex = 0;
     }
 
     private void updatePos() {
-
         moving = false;
 
-        if(left && !right) {
-            x-=playerSpeed;
+        if (left && !right) {
+            x -= playerSpeed;
             moving = true;
-        } else if(right && !left) {
-            x+=playerSpeed;
+        } else if (right && !left) {
+            x += playerSpeed;
             moving = true;
         }
-        if(up && !down) {
-            y-=playerSpeed;
+
+        if (up && !down) {
+            y -= playerSpeed;
             moving = true;
-        } else if(down && !up) {
-            y+=playerSpeed;
+        } else if (down && !up) {
+            y += playerSpeed;
             moving = true;
         }
     }
 
     private void loadAnimations() {
 
-            BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
+        BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
 
-            animations = new BufferedImage[9][6];
-            for(int j = 0; j < animations.length; j++)
-                for(int i = 0; i < animations[j].length; i++)
-                    animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
-
+        animations = new BufferedImage[9][6];
+        for (int j = 0; j < animations.length; j++)
+            for (int i = 0; i < animations[j].length; i++)
+                animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
 
     }
 
@@ -107,20 +109,20 @@ public class Player extends Entity {
         this.attacking = attacking;
     }
 
-    public boolean isUp() {
-        return up;
-    }
-
-    public void setUp(boolean up) {
-        this.up = up;
-    }
-
     public boolean isLeft() {
         return left;
     }
 
     public void setLeft(boolean left) {
         this.left = left;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
     }
 
     public boolean isRight() {
@@ -138,6 +140,5 @@ public class Player extends Entity {
     public void setDown(boolean down) {
         this.down = down;
     }
+
 }
-
-
