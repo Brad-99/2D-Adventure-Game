0b9f83ff0a191b476fa0cc6e23f7194ac2abec7a
BradpuppyC <rayray21211@gmail.com>
2024-01-24 23:06:02 +0000

    9d

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -4,10 +4,8 @@ import static utilz.Constants.PlayerConstants.*;
 import static utilz.HelpMethods.CanMoveHere;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
-import javax.imageio.ImageIO;
 
+import main.Game;
 import utilz.LoadSave;
 
 public class Player extends Entity {
@@ -18,21 +16,24 @@ public class Player extends Entity {
     private boolean left, up, right, down;
     private float playerSpeed = 2.0f;
     private int[][] lvlData;
+    private float xDrawOffset = 21 * Game.SCALE;
+    private float yDrawOffset = 4 * Game.SCALE;
 
     public Player(float x, float y, int width, int height) {
         super(x, y, width, height);
         loadAnimations();
+        initHitbox(x, y, 20 * Game.SCALE, 28 * Game.SCALE);
+
     }
 
     public void update() {
         updatePos();
-        updateHitbox();
         updateAnimationTick();
         setAnimation();
     }
 
     public void render(Graphics g) {
-        g.drawImage(animations[playerAction][aniIndex], (int) x, (int) y, width, height, null);
+        g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset), (int) (hitbox.y - yDrawOffset), width, height, null);
         drawHitbox(g);
     }
 
@@ -72,26 +73,33 @@ public class Player extends Entity {
 
     private void updatePos() {
         moving = false;
-        if(!left && !right && !up && !down)
+        if (!left && !right && !up && !down)
             return;
 
         float xSpeed = 0, ySpeed = 0;
 
         if (left && !right)
             xSpeed = -playerSpeed;
-         else if (right && !left)
+        else if (right && !left)
             xSpeed = playerSpeed;
 
         if (up && !down)
             ySpeed = -playerSpeed;
-         else if (down && !up)
+        else if (down && !up)
             ySpeed = playerSpeed;
 
-         if(CanMoveHere(x+xSpeed, y+ySpeed, width, height, lvlData)) {
-             this.x += xSpeed;
-             this.y += ySpeed;
-             moving = true;
-         }
+//		if (CanMoveHere(x + xSpeed, y + ySpeed, width, height, lvlData)) {
+//			this.x += xSpeed;
+//			this.y += ySpeed;
+//			moving = true;
+//		}
+
+        if (CanMoveHere(hitbox.x + xSpeed, hitbox.y + ySpeed, hitbox.width, hitbox.height, lvlData)) {
+            hitbox.x += xSpeed;
+            hitbox.y += ySpeed;
+            moving = true;
+        }
+
     }
 
     private void loadAnimations() {
